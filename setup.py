db1f1b43aff2b4f88d0aa87bd8493c2bcc00ab70
Furkan <141925837+CreativexName@users.noreply.github.com>
2025-07-03 18:30:39 +0200

    Upped version number (forgot previously)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='latexit',
-    version='0.1.0',
+    version='0.2.0',
     description='Render LaTeX code to PNG images with transparent background (no math mode required)',
     author='Furkan Tandogan',
     packages=find_packages(),
